bf7edbaab291035fd29c78696e8742f4aab12372
Sagun Pai <sagung.pai@gmail.com>
2016-02-22 20:11:44 +0530

    Basic shiny app

---
+++ webapp/server.R
@@ -0,0 +1,14 @@
+# server.R
+
+shinyServer(
+  function(input, output) {
+  
+    output$text1 <- renderText({ 
+      paste("You have selected", input$var)
+    })
+    output$text2 <- renderText({ 
+      paste("Range", input$range[1], "to", input$range[2])
+    })
+    
+  }
+)
